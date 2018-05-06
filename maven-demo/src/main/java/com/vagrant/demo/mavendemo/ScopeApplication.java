cb3566ce8fd0c55f948c0743204be16e1527baa5
Martin Tan <Martin-Tan@users.noreply.github.com>
2018-05-06 20:08:45 +0800

    binding type: scope proxy mode

--- maven-demo/src/main/java/com/vagrant/demo/mavendemo/ScopeApplication.java
+++ maven-demo/src/main/java/com/vagrant/demo/mavendemo/ScopeApplication.java
@@ -1,25 +1,29 @@
 package com.vagrant.demo.mavendemo;
 
 import com.vagrant.demo.mavendemo.scope.PersonDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ScopeApplication {
-
+    private static Logger LOGGER = LoggerFactory.getLogger(ScopeApplication.class);
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 SpringApplication.run(ScopeApplication.class, args);
 
         PersonDao personDao =
                 applicationContext.getBean(PersonDao.class);
-        System.out.println(personDao);
+        LOGGER.info("{}", personDao);
+        LOGGER.info("{}", personDao.getJdbConnection());
 
 
         PersonDao personDao2 =
                 applicationContext.getBean(PersonDao.class);
-        System.out.println(personDao2);
+        LOGGER.info("{}", personDao2);
+        LOGGER.info("{}", personDao2.getJdbConnection());
 
     }
 }
--- maven-demo/src/main/java/com/vagrant/demo/mavendemo/scope/JdbConnection.java
+++ maven-demo/src/main/java/com/vagrant/demo/mavendemo/scope/JdbConnection.java
@@ -2,9 +2,12 @@ package com.vagrant.demo.mavendemo.scope;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class JdbConnection {
     public JdbConnection() {
         System.out.println("JDB Connection");
